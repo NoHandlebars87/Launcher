d7793d46bb24e390122da4f6ef69602b02cde670
Pazaz <pazaz@itspazaz.com>
2025-01-28 03:59:28 -0500

    feat: F5-refresh, disable throttling, limit to allowed domains

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { URL } = require('url');
+const { app, BrowserWindow, Menu, powerSaveBlocker } = require('electron');
 const { updateElectronApp } = require('update-electron-app');
 const windowStateKeeper = require('electron-window-state');
 
@@ -23,7 +24,11 @@ function createWindow() {
         y: mainWindowState.y,
         width: mainWindowState.width,
         height: mainWindowState.height,
-        show: false
+        show: false,
+        webPreferences: {
+            pageVisibility: true,
+            backgroundThrottling: false
+        }
     });
     mainWindowState.manage(win);
 
@@ -71,17 +76,24 @@ function createWindow() {
                 event.preventDefault();
 
                 win.loadURL('https://2004.lostcity.rs/title');
-            } else if (input.key === 'F11') {
-                // toggle fullscreen
-                event.preventDefault();
-
-                win.setFullScreen(!win.isFullScreen());
             }
+        } else if (input.key === 'F5') {
+            // reload
+            event.preventDefault();
+
+            win.reload();
+        } else if (input.key === 'F11') {
+            // toggle fullscreen
+            event.preventDefault();
+
+            win.setFullScreen(!win.isFullScreen());
         }
     });
 }
 
 app.whenReady().then(() => {
+    powerSaveBlocker.start('prevent-display-sleep');
+
     createWindow();
 
     app.on('activate', () => {
@@ -95,4 +107,14 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
+});
+
+app.on('web-contents-created', (event, contents) => {
+    contents.on('will-navigate', (event, navigationUrl) => {
+        const parsedUrl = new URL(navigationUrl)
+
+        if (parsedUrl.origin !== 'https://lostcity.rs' && !parsedUrl.origin.endsWith('.lostcity.rs')) {
+            event.preventDefault();
+        }
+    });
 });
